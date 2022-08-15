37431f98fd826adff6aa94c0c6de8bfc67fd5bde
aR <abenezerkifle000@gmail.com>
2022-08-15 14:08:13 +0300

    Add search bar

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -43,7 +43,7 @@ function Navbar() {
               {MobileMenu ? (
                 <i className="fas fa-times close home-btn"></i>
               ) : (
-                <i className="fa-solid fa-xmark open"></i>
+                <i className="fa-solid fa-bars open"></i>
               )}
             </button>
           </div>
