8436e36a954b15d664de6ae81a19d8758ede24d8
aR <abenezerkifle000@gmail.com>
2022-08-15 14:47:54 +0300

    Adding pages for main pages

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import logo from "../../components/assets/images/logo.svg";
-const Search = () => {
- 
-    window.addEventListener("scroll", function(){
-      const search = this.document.querySelector(".search")
-      search.classList.toggle("active", this.window.scrollY > 100)
-    })
-  
+import React from "react"
+import logo from "../../components/assets/images/logo.svg"
+
+
+const Search = ({ CartItem }) => {
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const search = document.querySelector(".search")
+    search.classList.toggle("active", window.scrollY > 100)
+  })
+
   return (
     <>
-      <section className="search">
-        <div className="container c_flex">
-          <div className="logo width ">
-            <img src={logo} alt="" />
+      <section className='search'>
+        <div className='container c_flex'>
+          <div className='logo width '>
+            <img src={logo} alt='' />
           </div>
 
-          <div className="search-box f_flex">
-            <i className="fa fa-search"></i>
-            <input type="text" placeholder="Search and hit enter..." />
+          <div className='search-box f_flex'>
+            <i className='fa fa-search'></i>
+            <input type='text' placeholder='Search and hit enter...' />
             <span>All Category</span>
           </div>
 
-          <div className="icon f_flex width">
-            <i className="fa fa-user icon-circle"></i> 
-            <div className="cart">
+          <div className='icon f_flex width'>
+            <i className='fa fa-user icon-circle'></i>
+            <div className='cart'>
               <a href="/cart">
-                <i className="fa fa-shopping-bag icon-circle"> </i>
+                <i className='fa fa-shopping-bag icon-circle'></i>
                 <span>0</span>
               </a>
             </div>
@@ -33,7 +35,7 @@ const Search = () => {
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
