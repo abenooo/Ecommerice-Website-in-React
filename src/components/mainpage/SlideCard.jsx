150a4fc686327907cf3de9fdff2f8c0f5ffee5bd
aR <abenezerkifle000@gmail.com>
2022-08-15 17:02:09 +0300

    Sidebar fixed

--- src/components/mainpage/SlideCard.jsx
+++ src/components/mainpage/SlideCard.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Sdata from "./Sdata";
 
 function SlideCard() {
   return (
     <>
-      <h1>SlideCard</h1>
+      {Sdata.map((value, index) => {
+        return (
+          <div className="box d_flex top" key={index}>
+            <div className="left">
+              <h1>{value.title}</h1>
+              <p>{value.desc}</p>
+              <button className="btn-primary">Visit Collection</button>
+            </div>
+            <div className="right">
+              <img src={value.cover} alt="" />
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
