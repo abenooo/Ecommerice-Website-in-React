9acdd16caf22c5c3126df1f3e57564f16f54b64e
aR <abenezerkifle000@gmail.com>
2022-08-16 09:36:37 +0300

    Fineshed slider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 // import "./App.css";
 import "./common/header/Header.css";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./common/header/Header";
 import Pages from "./pages/Pages";
+import Data from "./components/flashDeals/Data";
 
 function App() {
+  // step 1 fetch data and use state pass it
+  const { productItems } = Data;
+  const [cartItem, setCartItem] = useState([]);
   return (
-      <>
+    <>
       <Router>
         <Header />
         <Switch>
-          <Route path='/' exact>
-            <Pages/>
+          <Route path="/" exact>
+            <Pages productItems= {productItems}/>
           </Route>
         </Switch>
       </Router>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import FlashDeals from "../components/flashDeals/FlashDeals";
 import Home from "../components/mainpage/Home";
 
-function Pages() {
+const Pages = ({ productItems }) => {
   return (
     <>
       <Home />
+      <FlashDeals productItems={productItems} />
     </>
   );
-}
+};
 
 export default Pages;
