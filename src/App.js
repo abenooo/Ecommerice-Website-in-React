ce5c45c2e09041dbbd40b52a35737c7f3b83ca10
aR <abenezerkifle000@gmail.com>
2022-08-16 15:11:21 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./common/header/Header";
+import Footer from "./common/footer/Footer";
 import Pages from "./pages/Pages";
 import Data from "./components/flashDeals/Data";
 
 function App() {
   // step 1 fetch data and use state pass it
   const { productItems } = Data;
-  const [cartItem, setCartItem] = useState([]);
+  // const [cartItem, setCartItem] = useState([]);
   return (
     <>
       <Router>
         <Header />
         <Switch>
           <Route path="/" exact>
-            <Pages productItems= {productItems}/>
-          </Route>
-        </Switch>
-      </Router>
+            <Pages productItems={productItems} />{" "}
+          </Route>{" "}
+        </Switch>{" "}
+        <Footer />
+      </Router>{" "}
     </>
   );
 }
